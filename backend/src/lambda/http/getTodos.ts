66483fa63a1cfed552f0927996df8205c2fb657b
MosesSoftEng <moses.soft.eng@gmail.com>
2022-10-12 22:01:16 +0300

    Fix syntax error

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -5,15 +5,15 @@ import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 
 import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
-import { getUserId } from '../utils';
+import { getUserId } from '../utils'
 
 // TODO: Get all TODO items for a current user
-export const handler = middy(
-  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     // Write your code here
     const todos = '...'
 
     return undefined
+})
 
 handler.use(
   cors({
