3cae61c9a90d28e600f4c0c286eaa4cb3f03ca7b
MosesSoftEng <moses.soft.eng@gmail.com>
2022-10-15 09:51:01 +0300

    Complete create function

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -4,29 +4,33 @@ import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 import { getUserId } from '../utils';
-import { createTodo } from '../../businessLogic/todos'
+import { createTodo } from '../../helpers/todos'
+
 
 export const handler = middy(
-  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const newTodo: CreateTodoRequest = JSON.parse(event.body)
-    // TODO: Implement creating a new TODO item
-    const userId = getUserId(event);
-    const newItem = await createTodo(newTodo, userId)
+	async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+		const newTodo: CreateTodoRequest = JSON.parse(event.body)
+		// TODO: Implement creating a new TODO item
+		const userId = getUserId(event);
 
-    return {
-      statusCode: 201,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true
-      },
-      body: JSON.stringify({
-        item: newItem
-      })
-    }
-  })
+		// Function in business logic
+		const newItem = await createTodo(newTodo, userId)
+
+		return {
+			statusCode: 201,
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Credentials': true
+			},
+			body: JSON.stringify({
+				item: newItem
+			})
+		}
+	}
+)
 
 handler.use(
-  cors({
-    credentials: true
-  })
+	cors({
+		credentials: true
+	})
 )
--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -4,15 +4,38 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 
-import { deleteTodo } from '../../businessLogic/todos'
+import { deleteTodo } from '../../helpers/todos'
 import { getUserId } from '../utils'
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const todoId = event.pathParameters.todoId
-    // TODO: Remove a TODO item by id
-    
-    return undefined
+    const todoId = event.pathParameters.todoId;
+	// TODO: Remove a TODO item by id
+
+    const userId = getUserId(event);
+    const response = await deleteTodo(todoId, userId)
+
+	if (response) {
+		return {
+			statusCode: 204,
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Credentials': true
+			},
+			body: JSON.stringify({})
+		}
+	}
+
+	return {
+		statusCode: 404,
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Credentials': true
+		},
+		body: JSON.stringify({
+			error: 'Todo not found'
+		})
+	}
   }
 )
 
